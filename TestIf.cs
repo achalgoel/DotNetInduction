756b93380e7b57a388e4f3299e706ed71733d811
achalgoel <achalgoel23@gmail.com>
2016-07-05 17:07:07 +0530

    assignment 1 completed TestIf

---
+++ TestIf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your CHOICE of language :");
+            Console.WriteLine("1. VB.Net");
+            Console.WriteLine("2. C# :");
+            Console.WriteLine("3. Any other");
+            Console.Write("Enter here :");
+            int choice;
+            choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                Console.WriteLine(" VB .NET: OOP, multithreading and more!");
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("C# is a great language");
+            }
+            else 
+            {
+                Console.WriteLine("Good Luck with hat !");
+            }
+        }
+    }
+}
